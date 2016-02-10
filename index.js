10fc28f2dbbebcaaa8ef338d89755514319c543d
Danny Coates <dannycoates@gmail.com>
2016-02-09 21:26:00 -0800

    format date and t

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 
 var JSONStream = require('json-stream')
 var through = require('through2')
+var dateformat = require('dateformat')
 
 process.stdin
   .pipe(JSONStream())
   .pipe(
     through.obj(
       function (obj, _, next) {
+        var time = new Date(obj.time)
         var stat = {
-          time: obj.time,
+          time: dateformat(time, 'yyyy-mm-dd HH:MM:ss'),
           agent: obj.agent,
           method: obj.method,
           path: obj.path,
           code: obj.code,
-          t: obj.request_time,
+          t: Math.floor(obj.request_time * 1000),
           host: obj.hostname
         }
         next(null, JSON.stringify(stat) + '\n')
